c247fa91fa2f6a4797ecd5bb3d4586c4204f4693
voravich1 <voravich1@hotmail.com>
2018-12-19 15:47:49 +0700

    Add files via upload

---
+++ src/annotb/Annotb.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package annotb;
+
+/**
+ *
+ * @author worawich
+ */
+public class Annotb {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
